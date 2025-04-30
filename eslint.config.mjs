3787e77de3c65b7b537dc0c550493ec3a0c01409
Kaitlyn Bricker <kaitbricker@gmail.com>
2025-04-30 14:28:21 -0400

    chore: update ESLint config to allow @ts-expect-error without description

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/ban-ts-comment": ["error", {
+        "ts-expect-error": "allow-with-description",
+        "ts-ignore": true,
+        "ts-nocheck": true,
+        "ts-check": false,
+        "minimumDescriptionLength": 0
+      }]
+    }
+  }
 ];
 
 export default eslintConfig;
